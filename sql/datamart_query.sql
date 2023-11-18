c3ca60e89a3c2ab12ec2b73b2079fbf542f398b7
Peter Petrov <p.petrov@postgrespro.ru>
2023-11-18 12:29:34 +0300

    Modify result alignment

--- sql/datamart_query.sql
+++ sql/datamart_query.sql
@@ -13,16 +13,16 @@ ANALYZE analysis.dm_rfm_segments;
 
 SELECT d.* FROM analysis.dm_rfm_segments d ORDER BY d.user_id;
 
-'''
-user_id  recency frequency monetary_value
-0	 1	 3	   4
-1	 4	 3	   3
-2	 2	 3	   5
-3	 2	 3         3
-4	 4	 3	   3
-5	 4	 5	   5
-6	 1	 3	   5
-7	 4	 2	   2
-8	 1	 1	   3
-9	 1	 2	   2
-'''
+/*
+user_id recency frequency monetary_value
+0	      1	      3	        4
+1	      4	      3	        3
+2	      2	      3	        5
+3	      2	      3         3
+4	      4	      3	        3
+5	      4	      5	        5
+6	      1	      3	        5
+7	      4	      2	        2
+8	      1	      1	        3
+9	      1	      2	        2
+*/
